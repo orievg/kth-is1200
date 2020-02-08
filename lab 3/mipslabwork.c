e196146e84d1c8fb39d1745760013d98688a1cf6
Dalvie Benu <dalviebenu@Dalvies-MBP.lan>
2020-02-08 21:23:54 +0100

    finished question 1.

--- lab 3/mipslabwork.c
+++ lab 3/mipslabwork.c
@@ -6,7 +6,7 @@
    This file should be changed by YOU! So you must
    add comment(s) here with your name(s) and date(s):
 
-   This file modified 2017-04-31 by Ture Teknolog 
+   This file modified 2017-04-31 by Ture Teknolog
 
    For copyright and licensing, see file COPYING */
 
@@ -28,9 +28,10 @@ void user_isr( void )
 void labinit( void )
 {
 	int* led;
-	led = 0xbf886100;
-  	*led &= ~ 0xff; // set 8 lsb of PORTE to 0 (output)
+  led = 0xbf886100;
+  *led &= ~ 0xff; // set 8 lsb of PORTE to 0 (output)
 
+  TRISD &= 0x7f0; // set bits 7-11 as 1 (intputs)
 
 }
 
@@ -38,6 +39,8 @@ void labinit( void )
 void labwork( void )
 {
 	int* out;
+  volatile int val;
+  volatile int btns;
 	out =  0xbf886110;
 	*out = 0;
 
@@ -49,4 +52,22 @@ void labwork( void )
   	*out++;
   	display_image(96, icon);
 
+    while(1){
+      // checks push btns and updates appropriate digit.
+      btns = getbtns();
+      val = getsw();
+
+      if ((btns && 0x1) == 1) {
+        val = val || 0xff0f; // sets all other bits to 1.
+        mytime = (mytime || 0x00f0) && val; // copies val to mytime
+      }
+      if (((btns && 0x2) >> 1) == 1) {
+        val = val || 0xf0ff; // sets all other bits to 1.
+        mytime = (mytime || 0x0f00) && val; // copies val to mytime
+      }
+      if (()(btns && 0x4) >> 2) == 1) {
+        val = val || 0x0fff; // sets all other bits to 1.
+        mytime = (mytime || 0xf000) && val; // copies val to mytime
+      }
+    }
 }
