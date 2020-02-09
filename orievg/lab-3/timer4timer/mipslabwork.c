9fe1a658cf9317089bbd92c37c7dbe62d34a1220
orievg <orievg@yandex.ru>
2020-02-10 00:10:45 +0100

    Assignment 2 of lab 3

--- orievg/lab-3/timer4timer/mipslabwork.c
+++ orievg/lab-3/timer4timer/mipslabwork.c
@@ -13,9 +13,8 @@
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
-
 int mytime = 0x5957;
-
+int timeoutcount = 0;
 char textstring[] = "text, more text, and even more text!";
 
 /* Interrupt Service Routine */
@@ -27,22 +26,30 @@ void user_isr( void )
 /* Lab-specific initialization goes here */
 void labinit( void ){
   volatile int* E= (volatile int*) 0xbf886100;
+
+  volatile int* pE= (volatile int*) 0xbf886110;
+  *pE = 0x0;
+  *pE= *pE+1;
   *E = * E & 0xff00; //init port E
-  TRISD = TRISD & 0xfe0; // init port D
+  TRISD = TRISD & 0x0fe0; // init port D
+
+  T2CONCLR =0x0; //disable timer b4 configuring
+
+
+  T2CONSET = 0x70; //prescale 256 //0111
+  TMR2 = 0x0; //set timer to 0
+  PR2 = (8000000 / 256); // AKA 80 mil /256/10
+  T2CONSET = 0x8000; //timer on
+
   return;
 }
 
 /* This function is called repetitively from the main program */
 void labwork( void ){
-  volatile int* pE= (volatile int*) 0xbf886110;
-  *pE = 0;
+
   int btn = getbtns();
   int sw = getsw();
 
-  while (1) {
-    delay( 1000 );
-    btn = getbtns();
-    sw = getsw();
     //button 4 // 100,101,110,111
       if (btn ==4||btn ==5||btn ==6||btn ==7) {
         mytime = (mytime & 0x0fff) | (sw<<12);
@@ -55,12 +62,20 @@ void labwork( void ){
       if (btn ==1||btn ==3||btn ==5||btn ==7) {// 100,101,110,111
         mytime = (mytime & 0xff0f) | (sw<<4);
       }
-      time2string( textstring, mytime );
-      display_string( 3, textstring );
-      display_update();
-      tick( &mytime );
-      display_image(96, icon);
-      *pE= *pE+1;
+      if(IFS(0) & 0x100){/*Test time-out event flag*/
+        timeoutcount++;
+        IFSCLR(0) = 0x100;
 
-  }
+      }
+      if (timeoutcount==10) {
+        time2string( textstring, mytime );
+        display_string( 3, textstring );
+        display_update();
+        tick( &mytime );
+        display_image(96, icon);
+        timeoutcount=0;
+      }
+
+
+
 }
